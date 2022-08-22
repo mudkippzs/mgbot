294ce0b9676eb7f8ffe60e0476f51a22e293fe87
Mud <mudkippzs@gmail.com>
2022-08-22 16:52:39 +0100

    fixed typos.

--- cogs/boostcog.py
+++ cogs/boostcog.py
@@ -14,6 +14,37 @@ from clogger import clogger
 from utils import *
 
 
+async def post_to_cringe_gallery(payload, client):
+    """Get the original message content, embeds and attachments and post and Embed with the message author's details in the cringe gallery channel."""
+    clogger(f"Cringe was posted...")
+    cringe_gallery_channel_id = 1011283927924744212
+    message_content = ""
+    message_embeds = []
+    message_attachments = []
+
+    # Get the original message content, embeds and attachments
+    guild = client.get_guild(payload.guild_id)
+    message = await guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
+    author = message.author
+    message_content = message.content
+    message_embeds = message.embeds
+    message_attachments = message.attachments
+
+    # Create a new embed with the original message content, embeds and attachments
+    cringe_gallery_embed = discord.Embed(title=f"{author.display_name} just posted cringe in {message.channel.name}...", description=f"{author.mention}", color=0xffc0cb)
+    cringe_gallery_embed.add_field(name="Cringe:", value=message_content, inline=False)
+
+    if len(message_embeds) > 0:
+        for e in message_embeds:
+            cringe_gallery_embed.add_field(name="Cringe Embeds", value=e, inline=False)
+    
+    if len(message_attachments) > 0:
+        for a in message_attachments:
+            cringe_gallery_embed.add_field(name="Cringe Attachments", value=a, inline=False)
+
+    # Post the new embed to the cringe gallery channel
+    await client.get_channel(cringe_gallery_channel_id).send(embed=cringe_gallery_embed)
+
 class Boostcog(commands.Cog):
     def __init__(self, client):
         config = load_json_config("config.json")
@@ -101,6 +132,7 @@ class Boostcog(commands.Cog):
 
 
         elif str(self.cringe_emoji.name) == str(payload.emoji.name):
+            await post_to_cringe_gallery(payload, self.client)
             if str(payload.message_id) not in [m[0] for m in self.basedlog[str(user_id)]["cringe_react_log"]] and str(payload.user_id) not in [m[1] for m in self.basedlog[str(user_id)]["cringe_react_log"]] or len(self.basedlog[str(user_id)]["cringe_react_log"]) < 1:
                 #clogger(
                 #    f"User {user_id} reacted with cringe on message {message_id}")
