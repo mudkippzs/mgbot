7c689f0664e3403b6bf4908aad742f2925bb36a3
Mud <mudkippzs@gmail.com>
2022-09-04 20:59:59 +0100

    fixed mathology for the rt monitor.

--- cogs/rtmon.py
+++ cogs/rtmon.py
@@ -49,7 +49,7 @@ def get_audience_score(soup):
 def get_audience_reviews(soup):
     audience_reviews = soup.find(
         'strong', attrs={'data-qa': 'audience-rating-count'})
-    return int(audience_reviews.text.strip().replace("User Ratings: ",""))
+    return int(audience_reviews.text.strip().replace("User Ratings: ", ""))
 
 
 def get_movie_image(soup):
@@ -68,72 +68,118 @@ class RTMon(commands.Cog):
         self.client = client
         self.historical_scores = []
         self.monitor_rt.start()
+        self.url = None
+        self.active = False
+        self.channel = None
 
     def cog_unload(self):
         self.monitor_rt.cancel()
 
+    @commands.slash_command(name="rttarget", description="Set the target for the RT monitor")
+    @commands.has_role('Admin')
+    async def rttarget(self, ctx, url: str = None):
+        if url == None:
+            await ctx.send_response("```Please provide a Rotten Tomatos URL. Ex: https://www.rottentomatoes.com/tv/the_lord_of_the_rings_the_rings_of_power/s01```", delete_after=15)
+
+        if url.startswith("https") == False:
+            await ctx.send_response("```Please provide a Rotten Tomatos URL. Ex: https://www.rottentomatoes.com/tv/the_lord_of_the_rings_the_rings_of_power/s01```", delete_after=15)
+
+        self.url = url
+        await ctx.send_response(f"```RT Monitor URL updated: {url}```", delete_after=10)
+
+    @commands.slash_command(name="rtchannel", description="Set the target Channel the RT monitor to send updates to.")
+    @commands.has_role('Admin')
+    async def rtchannel(self, ctx, channel: discord.channel.TextChannel = None):
+        if channel == None:
+            await ctx.send_response("```Please provide a channel ID or #channel_name```", delete_after=15)
+
+        self.channel = channel
+        await ctx.send_response(f"```RT Monitor Channel updated: {channel}```", delete_after=10)
+
+    @commands.slash_command(name="rttoggle", description="Toggle the updater ON or OFF, for the RT monitor")
+    @commands.has_role('Admin')
+    async def rttoggle(self, ctx, url: str = None):
+        if self.active == False:
+            self.active = True
+            state = "ON"
+        else:
+            self.active = False
+            state = "OFF"
+
+        await ctx.send_response(f"```RT Monitor: {state}```", delete_after=15)
+
     @tasks.loop(minutes=20)
     async def monitor_rt(self):
-        #clogger("Checking RT Reviews")
-        url = 'https://www.rottentomatoes.com/tv/the_lord_of_the_rings_the_rings_of_power/s01'
-        html = await get_html(url)
-        soup = get_soup(html)
+        if self.active:
+            if self.channel:
+                if self.url:
+                    #clogger("Checking RT Reviews")
+                    url = self.url
+                    html = await get_html(url)
+                    soup = get_soup(html)
 
-        critic_score = get_critic_score(soup)
-        critic_reviews = get_critic_reviews(soup)
-        audience_score = get_audience_score(soup)
-        audience_reviews = get_audience_reviews(soup)
-        movie_image = get_movie_image(soup)
-        movie_title = get_movie_title(soup)
+                    critic_score = get_critic_score(soup)
+                    critic_reviews = get_critic_reviews(soup)
+                    audience_score = get_audience_score(soup)
+                    audience_reviews = get_audience_reviews(soup)
+                    movie_image = get_movie_image(soup)
+                    movie_title = get_movie_title(soup)
 
-        audience_delta = 0
-        critic_delta = 0
+                    audience_delta = 0
+                    critic_delta = 0
 
-        self.historical_scores.append(
-            {
-                'critic_score': critic_score,
-                'critic_reviews': critic_reviews,
-                'audience_score': audience_score,
-                'audience_reviews': audience_reviews
-            }
-        )
+                    self.historical_scores.append(
+                        {
+                            'critic_score': critic_score,
+                            'critic_reviews': critic_reviews,
+                            'audience_score': audience_score,
+                            'audience_reviews': audience_reviews
+                        }
+                    )
 
-        clogger((movie_title, critic_score, critic_reviews,
-                 audience_score, audience_reviews))
+                    #clogger((movie_title, critic_score, critic_reviews,
+                    #         audience_score, audience_reviews))
 
-        if len(self.historical_scores) > 1:
-            critic_delta = int(self.historical_scores[-1]['critic_score']) - int(
-                self.historical_scores[-2]['critic_score'])
-            if critic_delta > 0:
-                critic_delta = f'+{critic_delta}'
+                    if len(self.historical_scores) > 1:
+                        #clogger(self.historical_scores)
+                        critic_delta = int(self.historical_scores[-1]['critic_reviews']) - int(
+                            self.historical_scores[-2]['critic_reviews'])
+
+                        if critic_delta != 0:
+                            critic_delta = f'{critic_delta}'
+                        else:
+                            critic_delta = str(critic_delta)
+
+                        audience_delta = int(self.historical_scores[-1]['audience_reviews']) - int(
+                            self.historical_scores[-2]['audience_reviews'])
+                        
+                        if audience_delta != 0:
+                            audience_delta = f'{audience_delta}'
+                        else:
+                            audience_delta = str(audience_delta)
+
+                    if int(critic_delta) != 0 or int(audience_delta) != 0:
+                        embed = discord.Embed(title="RT Cheater Monitor - Watching for deleted reviews...",
+                                              description=f'{movie_title}', color=0xff0000)
+                        embed.add_field(name='Critic Score',
+                                        value=f'**{critic_score}%** `({critic_reviews})`\t`Δ: {critic_delta}`', inline=False)
+                        embed.add_field(name='Audience Score',
+                                        value=f'**{audience_score}%** `({audience_reviews})`\t`Δ: {audience_delta}`', inline=False)
+                    else:
+                        embed = discord.Embed(title="RT Cheater Monitor - Watching for deleted reviews...",
+                                              description=f'{movie_title}', color=0x00ff00)
+                        embed.add_field(name='Critic Score',
+                                        value=f'**{critic_score}%** `({critic_reviews})`\t`Δ: --`', inline=False)
+                        embed.add_field(name='Audience Score',
+                                        value=f'**{audience_score}%** `({audience_reviews})`\t`Δ: --`', inline=False)
+                    embed.set_thumbnail(url=movie_image)
+
+                    channel = self.channel
+                    await channel.send(embed=embed)
+                else:
+                    clogger("```Please set a target URL.```")
             else:
-                critic_delta = str(critic_delta)
-
-            audience_delta = int(self.historical_scores[-1]['audience_score']) - int(
-                self.historical_scores[-2]['audience_score'])
-            if audience_delta > 0:
-                audience_delta = f'+{audience_delta}'
-            else:
-                audience_delta = str(audience_delta)
-
-        if int(critic_delta) != 0 or int(audience_delta) != 0:
-            embed = discord.Embed(title="RT Cheater Monitor - Watching for deleted reviews...",
-                              description=f'{movie_title}', color=0xff0000)
-            embed.add_field(name='Critic Score',
-                            value=f'**{critic_score}%** `({critic_reviews})`\t`Δ: {critic_delta}`', inline=False)
-            embed.add_field(name='Audience Score',
-                            value=f'**{audience_score}%** `({audience_reviews})`\t`Δ: {audience_delta}`', inline=False)
-        else:
-            embed = discord.Embed(title="RT Cheater Monitor - Watching for deleted reviews...",
-                              description=f'{movie_title}', color=0x00ff00)
-            embed.add_field(name='Critic Score',
-                            value=f'**{critic_score}%** `({critic_reviews})`\t`Δ: --`', inline=False)
-            embed.add_field(name='Audience Score',
-                            value=f'**{audience_score}%** `({audience_reviews})`\t`Δ: --`', inline=False)
-        embed.set_thumbnail(url=movie_image)
-
-        channel = self.client.get_channel(822886050359148556)
-        await channel.send(embed=embed)
+                clogger("```Please set a target channel for updates.```")
 
 
 def setup(client):
